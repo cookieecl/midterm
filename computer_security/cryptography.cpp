9c98dc04b1f8f4855bffbeaa6146c240d79dde83
anjel <angellineputri@gmail.com>
2025-06-06 05:33:47 +0800

    decipher

--- computer_security/cryptography.cpp
+++ computer_security/cryptography.cpp
@@ -15,15 +15,16 @@ char	letter_encrypter(char text, char key)
 {
 	int		text_number = text - 'A';
 	int 	key_number = key - 'A';
-	int 	result_number = text_number + key_number;
-	int		mod_result_number = result_number % 26;
-	char	result = mod_result_number + 'A';
 
-	// std::cout << "text: " << text << "(" << text_number << ")";
-	// std::cout << ", key: " << key << "(" << key_number << ")";
-	// std::cout << ", result: " << result_number << " % 26 = " << result << "(" << mod_result_number << ")" << std::endl;
+	return ((text_number + key_number) % 26 + 'A');
+}
 
-	return (result_number % 26 + 'A');
+char	letter_decrypter(char text, char key)
+{
+	int	text_number = text - 'A';
+	int key_number = key - 'A';
+
+	return ((text_number - key_number + 26) % 26 + 'A');
 }
 
 std::string	cipher(std::string plaintext, std::string key)
@@ -33,13 +34,13 @@ std::string	cipher(std::string plaintext, std::string key)
 	if (!strIsUpper(plaintext) || !strIsUpper(key))
 	{
 		std::cerr << "Ensure that both text and key only contain capital letters" << std::endl;
-		return 0;
+		return "";
 	}
 
 	if (key.length() < plaintext.length())
 	{
 		std::cerr << "Ensure that the key length is greater than or equal to the length of the text" << std::endl;
-		return 0;
+		return "";
 	}
 
 	for (int i = 0; i < plaintext.length(); ++i)
@@ -50,8 +51,36 @@ std::string	cipher(std::string plaintext, std::string key)
 	return (result);
 }
 
+std::string	decipher(std::string plaintext, std::string key)
+{
+	std::string	result;
+
+	if (!strIsUpper(plaintext) || !strIsUpper(key))
+	{
+		std::cerr << "Ensure that both text and key only contain capital letters" << std::endl;
+		return "";
+	}
+
+	if (key.length() < plaintext.length())
+	{
+		std::cerr << "Ensure that the key length is greater than or equal to the length of the text" << std::endl;
+		return "";
+	}
+
+	for (int i = 0; i < plaintext.length(); ++i)
+	{
+		result.push_back(letter_decrypter(plaintext[i], key[i]));
+	}
+
+	return (result);
+}
+
 int	main()
 {
-	std::cout << "/// Testing cipher(\"ANGELLINE\", \"THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM\") ///" << std::endl;
-	std::cout << "Result: " << cipher("ANGELLINE", "THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM") << std::endl;
+	std::cout << std::endl << "--- Testing cipher(\"ANGELLINE\", \"THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM\") ---" << std::endl;
+	std::string	cipher_result = cipher("ANGELLINE", "THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM");
+	std::cout << "Result: " << cipher_result << std::endl;
+
+	std::cout << std::endl << "--- Testing decipher((result of cipher function), \"THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM\") ---" << std::endl;
+	std::cout << "Result: " << decipher(cipher_result, "THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM") << std::endl;
 }
