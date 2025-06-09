5a3dd577bdddebe51fe72f3b046ed523f7202d67
Darren Fasius <angellineputri@gmail.com>
2025-06-09 17:59:10 +0800

    y

--- computer_security/cryptography.cpp
+++ computer_security/cryptography.cpp
@@ -33,21 +33,20 @@ std::string	cipher(std::string plaintext, std::string key)
 
 	if (!strIsUpper(plaintext) || !strIsUpper(key))
 	{
-		std::cerr << "Ensure that both text and key only contain capital letters" << std::endl;
+		std::cerr << "Ensure that both text and key only"
+			" contain capital letters" << std::endl;
 		return "";
 	}
-
 	if (key.length() < plaintext.length())
 	{
-		std::cerr << "Ensure that the key length is greater than or equal to the length of the text" << std::endl;
+		std::cerr << "Ensure that the key length is greater"
+			" than or equal to the length of the text" << std::endl;
 		return "";
 	}
-
 	for (int i = 0; i < plaintext.length(); ++i)
 	{
 		result.push_back(letter_encrypter(plaintext[i], key[i]));
 	}
-
 	return (result);
 }
 
@@ -57,30 +56,34 @@ std::string	decipher(std::string plaintext, std::string key)
 
 	if (!strIsUpper(plaintext) || !strIsUpper(key))
 	{
-		std::cerr << "Ensure that both text and key only contain capital letters" << std::endl;
+		std::cerr << "Ensure that both text and key only "
+			<< "contain capital letters" << std::endl;
 		return "";
 	}
-
 	if (key.length() < plaintext.length())
 	{
-		std::cerr << "Ensure that the key length is greater than or equal to the length of the text" << std::endl;
+		std::cerr << "Ensure that the key length is greater than or equal to the "
+			<< "length of the text" << std::endl;
 		return "";
 	}
-
 	for (int i = 0; i < plaintext.length(); ++i)
 	{
 		result.push_back(letter_decrypter(plaintext[i], key[i]));
 	}
-
 	return (result);
 }
 
 int	main()
 {
-	std::cout << std::endl << "--- Testing cipher(\"ANGELLINE\", \"THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM\") ---" << std::endl;
-	std::string	cipher_result = cipher("ANGELLINE", "THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM");
+	std::cout << std::endl << "--- Testing cipher(\"ANGELLINE\","
+		<< " \"THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM\") ---" << std::endl;
+	std::string	cipher_result = cipher("ANGELLINE", 
+								"THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM");
 	std::cout << "Result: " << cipher_result << std::endl;
 
-	std::cout << std::endl << "--- Testing decipher((result of cipher function), \"THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM\") ---" << std::endl;
-	std::cout << "Result: " << decipher(cipher_result, "THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM") << std::endl;
+	std::cout << std::endl << "--- Testing decipher((result of cipher function),"
+		<< "\"THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM\") ---" << std::endl;
+	std::cout << "Result: " << decipher(cipher_result, 
+						"THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM") << std::endl
+						<< std::endl;
 }
